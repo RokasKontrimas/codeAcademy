58c9a70aa45bed1afaebb54df6d167b1daa7e96c
RokasKontrimas <rokas.kontrimas123@gmail.com>
2024-03-13 11:45:03 +0200

    fixed boolean algebra

--- 2024-03-13/dom/index.js
+++ 2024-03-13/dom/index.js
@@ -8,24 +8,24 @@ let products = [
     {
         image: "https://beautinow.com/wp-content/uploads/2023/08/Most_Popular_Perfumes_0366584622ac4f4fa1abe0c937308f44.pngv=1691390378",
         category: "Perfume",
-        description: "A captivating and alluring perfume that embodies the essence of elegance and sophistication. This exquisite fragrance is carefully crafted to leave a lasting impression, evoking a sense of confidence and allure. With its unique blend of notes, Fragrance Essence is the perfect choice for those seeking a signature scent that is both timeless and unforgettable.",
+        description: "",
         title: "Fragrance Essence",
         price: "$139.99",
         priceOld: "$199.99",
     },
     {
         image: "https://t4.ftcdn.net/jpg/06/50/75/23/360_F_650752340_svHnkTNmdl7ISCeJMRrnyu9ZfDXLAkGT.webp",
-        category: "Perfume",
+        category: "",
         description: "Experience the enchantment of this luxurious perfume that captivates the senses and leaves a lasting impression. Enchanté Parfum is a harmonious blend of delicate floral notes and warm, sensual undertones, creating a truly mesmerizing fragrance. With its elegant and sophisticated aroma, Enchanté Parfum is the epitome of refined beauty and grace.\n",
         title: "Enchanté Parfum",
-        price: "$49.99",
+        price: "",
         priceOld: "$99.99",
     },
     {
         image: "",
         category: "Perfume",
         description: "Step into a world of pure bliss with Elysian Mist, a captivating perfume that transports you to a realm of enchantment and serenity. This exquisite fragrance combines ethereal floral notes with a touch of mystique, creating a scent that is both captivating and alluring. Elysian Mist is the perfect choice for those who seek a fragrance that embodies elegance, grace, and a touch of magic.",
-        title: "",
+        title: "Lorem ipsum perfume",
         price: "",
         priceOld: "$2000.00",
     },
@@ -35,13 +35,9 @@ let createProductList = (products) => {
     products.map((product) => {
 
 
-
-        if (!product.image && !product.title && !product.price) {
+        if (!product.image && !product.title || !product.title && !product.price) {
             console.log('lol')
         } else {
-            if (!product.image) {
-                product.image = 'https://png.pngtree.com/template/20220419/ourmid/pngtree-photo-coming-soon-abstract-admin-banner-image_1262901.jpg'
-            }
             let card = document.createElement('div')
             card.classList.add('card')
             cardList.appendChild(card)
@@ -51,7 +47,11 @@ let createProductList = (products) => {
             card.appendChild(cardImage)
 
             let productImage = document.createElement('img')
-            productImage.setAttribute('src', `${product.image}`)
+            if (!product.image) {
+                productImage.setAttribute('src', "https://png.pngtree.com/template/20220419/ourmid/pngtree-photo-coming-soon-abstract-admin-banner-image_1262901.jpg")
+            } else {
+                productImage.setAttribute('src', product.image)
+            }
             productImage.setAttribute('alt', `${product.title} image`)
             productImage.classList.add('product-image')
             cardImage.appendChild(productImage)
@@ -59,33 +59,44 @@ let createProductList = (products) => {
             let contentWrapper = document.createElement('div')
             contentWrapper.classList.add('content-wrapper')
             card.appendChild(contentWrapper)
+            if (!product.category) {
 
-            let productCategory = document.createElement('span')
-            productCategory.textContent = `${product.category}`
-            productCategory.classList.add('product-category')
-            contentWrapper.appendChild(productCategory)
+            } else {
+
+                let productCategory = document.createElement('span')
+                productCategory.textContent = product.category
+                productCategory.classList.add('product-category')
+                contentWrapper.appendChild(productCategory)
+            }
 
             let productTitle = document.createElement('h2')
-            productTitle.textContent = `${product.title}`
+            productTitle.textContent = product.title
             productTitle.classList.add('product-title')
             contentWrapper.appendChild(productTitle)
+            if (!product.description) {
 
-            let productDescription = document.createElement('p')
-            productDescription.textContent = `${product.description}`
-            productDescription.classList.add('product-description')
-            contentWrapper.appendChild(productDescription)
+            } else {
 
+                let productDescription = document.createElement('p')
+                productDescription.textContent = product.description
+                productDescription.classList.add('product-description')
+                contentWrapper.appendChild(productDescription)
+            }
             let productPriceWrapper = document.createElement('div')
             productPriceWrapper.classList.add('product-price-wrapper')
             contentWrapper.appendChild(productPriceWrapper)
 
             let productPrice = document.createElement('span')
-            productPrice.textContent = `${product.price}`;
+            productPrice.textContent = product.price
             productPrice.classList.add('product-price')
             productPriceWrapper.appendChild(productPrice)
-            if (product.priceOld) {
+
+
+            if (!product.priceOld) {
+            } else {
+                console.log(product.priceOld)
                 let productPriceOld = document.createElement('span')
-                productPriceOld.textContent = `${product.priceOld}`;
+                productPriceOld.textContent = product.priceOld
                 productPriceOld.classList.add('product-price-old')
                 productPriceWrapper.appendChild(productPriceOld)
             }
