003433d172dcf008d29b22fac43a07b7ae9cfbfb
RokasKontrimas <rokas.kontrimas123@gmail.com>
2024-04-11 13:04:14 +0300

    moved city form to component

--- 2024-04-03-react/cities/src/components/AddCityForm/FormComponent.js
+++ 2024-04-03-react/cities/src/components/AddCityForm/FormComponent.js
@@ -1,52 +1,113 @@
 import './FormComponent.css'
+import InputTextCombined from "./InputElements/InputTextCombined";
+import InputSelectionCombined from "./InputElements/InputSelectionCombined";
+import {useState} from "react";
 
-const FormComponent = () => {
+const FormComponent = (props) => {
+    const {onCitiesChange} = props
+    const DEFAULT_POPULATION = 1000;
+    const DEFAULT_CAPITAL_VALUE = false;
+    const [name, setName] = useState('')
+    const [population, setPopulation] = useState(DEFAULT_POPULATION);
+    const [continent, setContinent] = useState('')
+    const [country, setCountry] = useState('')
+    const [touristAttractions, setTouristAttractions] = useState('')
+    const [isCapital, setIsCapital] = useState(DEFAULT_CAPITAL_VALUE);
+    const createNewCity = () => {
+        let attractions = touristAttractions.split(',').map(item => item.trim());
+
+        const newCity = {
+            name: name,
+            population: population,
+            location: {
+                country: country,
+                continent: continent,
+            },
+            touristAttractions: attractions,
+            isCapital: isCapital,
+
+        }
+
+        onCitiesChange(newCity)
+
+        setName('')
+        setPopulation(DEFAULT_POPULATION)
+        setCountry('')
+        setContinent('')
+        setTouristAttractions('')
+        setIsCapital(DEFAULT_CAPITAL_VALUE)
+    }
     return (
         <div>
             <h1 className='title'>City form</h1>
-            <form onSubmit={(e) => {
+            <form id='city-form' onSubmit={(e) => {
                 e.preventDefault()
-                console.log(e.target.elements)
-
+                createNewCity()
             }}>
+                <InputTextCombined
+                    labelName='City'
+                    id='city-Name'
+                    type='text'
+                    inputName='city-name'
+                    dataState={name}
+                    onStateChange={setName}
+                />
+
+                <InputTextCombined
+                    labelName='Population'
+                    id='population'
+                    type='number'
+                    inputName='city-population'
+                    dataState={population}
+                    onStateChange={setPopulation}
+                />
+
+                <InputTextCombined
+                    labelName='Continent'
+                    id='continent'
+                    type='text'
+                    inputName='continent'
+                    dataState={continent}
+                    onStateChange={setContinent}
+                />
+
+                <InputTextCombined
+                    labelName='Country'
+                    id='country'
+                    type='text'
+                    inputName='country'
+                    dataState={country}
+                    onStateChange={setCountry}
+                />
                 <div className='form-control'>
-                    <label htmlFor='city'>
-                        City
+                    <label htmlFor='attractions'>
+                        Attractions
                     </label>
-                    <input id='city' type='text' name='city'/>
+                    <textarea
+                        id='attractions'
+                        value={touristAttractions}
+                        onChange={(e) => setTouristAttractions(e.target.value)}
+                    >
+            </textarea>
                 </div>
 
-                <div className='form-control'>
-                    <label htmlFor='population'>
-                        Population
-                    </label>
-                    <input id='population' type='number' name='population'/>
+                <div className='form-radio'>
+                    <InputSelectionCombined
+                        labelName='Yes'
+                        type='checkbox'
+                        id='is-capital'
+                        inputName='isCapital'
+                        stateValue={isCapital}
+                        onStateChange={setIsCapital}
+                    />
+
                 </div>
 
-                <div className='form-control'>
-                    <label htmlFor='continent'>
-                        Continent
-                    </label>
-                    <input id='continent' type='text' name='continent'/>
-                </div>
 
-                <div className='form-control'>
-                    <label htmlFor='country'>
-                        Country
-                    </label>
-                    <input id='country' type='text' name='country'/>
-                </div>
-
-                <div className='form-control'>
-                    <label htmlFor='tourist-attractions'>
-                        Tourist attractions
-                    </label>
-                    <input id='tourist-attractions' type='text' name='tourist-attractions'/>
-                </div>
-
-                <input type='submit' value='submit'/>
+                <button type='submit'>Create</button>
             </form>
         </div>
+
     )
 }
 export default FormComponent
--- 2024-04-03-react/cities/src/components/Main.js
+++ 2024-04-03-react/cities/src/components/Main.js
@@ -2,18 +2,10 @@ import {useState} from 'react'
 import HeaderComponent from "./HeaderItem/HeaderComponent";
 import CityComponent from "./CityItem/CityComponent";
 import './Main.css'
-import InputTextCombined from "./AddCityForm/InputElements/InputTextCombined";
-import InputSelectionCombined from "./AddCityForm/InputElements/InputSelectionCombined";
+import FormComponent from "./AddCityForm/FormComponent";
+
 
 const Main = () => {
-    const DEFAULT_POPULATION = 1000;
-    const DEFAULT_CAPITAL_VALUE = false;
-    const [name, setName] = useState('')
-    const [population, setPopulation] = useState(DEFAULT_POPULATION);
-    const [continent, setContinent] = useState('')
-    const [country, setCountry] = useState('')
-    const [touristAttractions, setTouristAttractions] = useState('')
-    const [isCapital, setIsCapital] = useState(DEFAULT_CAPITAL_VALUE);
     const [cities, setCities] = useState(
         [
             {
@@ -118,31 +110,9 @@ const Main = () => {
             }
         ]);
 
-
-    const createNewCity = () => {
-        let attractions = touristAttractions.split(',').map(item => item.trim());
-
-        const newCity = {
-            name: name,
-            population: population,
-            location: {
-                country: country,
-                continent: continent,
-            },
-            touristAttractions: attractions,
-            isCapital: isCapital,
-
-        }
-
+    const returnCities = (newCity) => {
 
         setCities(prevState => [newCity, ...prevState])
-
-        setName('')
-        setPopulation(DEFAULT_POPULATION)
-        setCountry('')
-        setContinent('')
-        setTouristAttractions('')
-        setIsCapital(DEFAULT_CAPITAL_VALUE)
     }
 
 
@@ -150,73 +120,7 @@ const Main = () => {
         <>
             <HeaderComponent/>
             <CityComponent cities={cities}/>
-            <div>
-                <h1 className='title'>City form</h1>
-                <form id='city-form' onSubmit={(e) => {
-                    e.preventDefault()
-                    createNewCity()
-                }}>
-                    <InputTextCombined
-                        labelName='City'
-                        id='city-Name'
-                        type='text'
-                        inputName='city-name'
-                        dataState={name}
-                        onStateChange={setName}
-                    />
-
-                    <InputTextCombined
-                        labelName='Population'
-                        id='population'
-                        type='number'
-                        inputName='city-population'
-                        dataState={population}
-                        onStateChange={setPopulation}
-                    />
-
-                    <InputTextCombined
-                        labelName='Continent'
-                        id='continent'
-                        type='text'
-                        inputName='continent'
-                        dataState={continent}
-                        onStateChange={setContinent}
-                    />
-
-                    <InputTextCombined
-                        labelName='Country'
-                        id='country'
-                        type='text'
-                        inputName='country'
-                        dataState={country}
-                        onStateChange={setCountry}
-                    />
-                    <div className='form-control'>
-                    <label htmlFor='attractions'>
-                        Attractions
-                    </label>
-                    <textarea id='attractions'
-                              value={touristAttractions}
-                              onChange={(e) => setTouristAttractions(e.target.value)}>
-            </textarea>
-                    </div>
-
-                    <div className='form-radio'>
-                        <InputSelectionCombined
-                            labelName='Yes'
-                            type='checkbox'
-                            id='is-capital'
-                            inputName='isCapital'
-                            stateValue={isCapital}
-                            onStateChange={setIsCapital}
-                        />
-
-                    </div>
-
-
-                    <button type='submit'>Create</button>
-                </form>
-            </div>
+            <FormComponent onCitiesChange={returnCities}/>
         </>
     )
 }
